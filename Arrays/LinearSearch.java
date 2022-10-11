4bade85d1c28734877050b977086c21bade78a2a
JatinderK325 <74372431+JatinderK325@users.noreply.github.com>
2022-10-10 23:05:22 -0400

    Linear search added

---
+++ Arrays/LinearSearch.java
@@ -0,0 +1,38 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class LinearSearch {
+    public static int[] takeInput(int arr[], Scanner sc) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        return arr;
+    }
+
+    public static int findElement(int array[], Scanner sc) {
+        int searchElement = sc.nextInt();
+        int index = -1 ;
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] == searchElement){
+                index = i;
+            }
+            else{
+                index = -1;
+            }
+        }
+        return index;
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the size of an array and ");
+        int size = s.nextInt();
+        int arr[] = new int[size];
+        System.out.println("Enter the elements of an array");
+        int array[] = takeInput(arr, s);
+        System.out.println("Enter the element you want to search");
+        int elementIndex = findElement(array, s);
+        System.out.println(elementIndex);
+    }
+}
