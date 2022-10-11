17b39059c9bb452e63eada29bf23180488b7bea4
JatinderK325 <74372431+JatinderK325@users.noreply.github.com>
2022-10-11 18:14:42 -0400

    Examples on Arrays

---
+++ Arrays/SelectionSort.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class SelectionSort {
+    public static int binarySearch(int array[], int elementToSearch) {
+        int start = 0;
+        int end = array.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (elementToSearch == array[mid]) {
+                return mid;
+            } else if (elementToSearch > array[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int input[] = { 2, 4, 5, 8, 9, 15, 21, 28 };
+        int index = binarySearch(input, 8);
+        System.out.println(index);
+}
