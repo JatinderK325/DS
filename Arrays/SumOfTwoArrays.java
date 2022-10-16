f0fd3ba09d9f359d4f5f27291127a31cc3d37530
JatinderK325 <74372431+JatinderK325@users.noreply.github.com>
2022-10-16 15:02:15 -0400

    code modified

--- Arrays/SumOfTwoArrays.java
+++ Arrays/SumOfTwoArrays.java
@@ -2,20 +2,57 @@ package Arrays;
 // incomplete
 public class SumOfTwoArrays {
     public static int[] add(int arr1[], int arr2[]){
-        int i = 0;
-        int j = 0;
-        int k = 0;
-        int ans[] = new int[arr1.length + 1];
-        while (i < arr1.length && j < arr2.length) {
-            ans[k+1] = arr1[i] + arr2[j];
-            //if(ans[k+1]){
+        int na = arr1.length;
+        int nb = arr2.length;
+        int nc;
+        int min;
 
-           // }
-            i++;
-            j++;
-            k++;
+        if(na > nb){
+            nc = na + 1;
+            min = nb;
+        }else{
+            nc = nb + 1;
+            min = na;
         }
-        return ans;
+
+        int c[] = new int[nc];
+        int sum = 0;
+        int carry = 0;
+        int i = na - 1;
+        int j = nb - 1;
+        int k = nc - 1;
+
+        while(i >= 0 && j>=0){
+            sum  = arr1[i] + arr2[j] + carry;
+            i--;
+            j--;
+            c[k] = sum % 10;
+            sum = sum / 10;
+            carry = sum % 10;
+            k--;        
+        }//end of while loop
+
+        while(i >= 0){          //n2 has exhausted
+            sum  = arr1[i] + carry;
+            c[k] = sum % 10;
+            sum = sum / 10;
+            carry = sum % 10;
+            i--;
+            k--;
+        }
+
+        while(j >= 0){        //n1 has exhausted  
+            sum  = arr2[j] + carry;
+            c[k] = sum % 10;
+            sum = sum / 10;
+            carry = sum % 10;
+            k--;        
+            j--;
+        }
+
+        c[k] = carry;
+
+        return c;
 
     }
     public static void main(String[] args) {
