fad89f971186bf2cfa50e6cbc7380bd1992c9506
JatinderK325 <74372431+JatinderK325@users.noreply.github.com>
2022-10-17 16:57:15 -0400

    Row wise sum in 2D array added

--- TwoDArrays/RowWiseSum.java
+++ TwoDArrays/RowWiseSum.java
@@ -30,26 +30,21 @@ public class RowWiseSum {
         }
     }
 
-    public static int largestColSum(int arr[][]){
+    public static void rowWiseSum(int arr[][]){
         int rows = arr.length;
         int cols = arr[0].length;
-        int largest = Integer.MIN_VALUE;
-        for (int j = 0; j < cols; j++) { //cols
+        for (int i = 0; i < rows; i++) { //rows
             int sum = 0;
-            for (int i = 0; i < rows; i++) { //rows
+            for (int j = 0; j < cols; j++) { //cols
                 sum = sum + arr[i][j];
-            }
-            if(sum > largest){
-                largest = sum;
-            }
+            }    
+            System.out.print(sum + " ");
         }
-        return largest;
     }
 
     public static void main(String[] args) {
         int arr[][] = takeInput();
         print2DArray(arr);
-        int largestSum = largestColSum(arr);
-        System.out.println(largestSum);
+        rowWiseSum(arr);
     }
 }
