1c168c929546e56c5ce0bac7f611b986b25b84fb
JatinderK325 <74372431+JatinderK325@users.noreply.github.com>
2022-10-30 20:16:47 -0400

    Fibonacci number

---
+++ ObjectOrientedProgramming/Recursion/FibonacciNumber.java
@@ -0,0 +1,14 @@
+package ObjectOrientedProgramming.Recursion;
+
+public class FibonacciNumber {
+    public static int fib(int n){
+        if(n == 0 || n == 1){
+            return n;
+        }
+        return fib(n-1) + fib(n-2);
+    }
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println(fib(n));
+    }
+}
