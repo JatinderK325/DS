b3b368735282142eeeff4514da651c774c893c3d
JatinderK325 <jatinder1994k@gmail.com>
2022-11-12 16:54:15 -0500

    comment

--- Strings/CountWords.java
+++ Strings/CountWords.java
@@ -7,7 +7,7 @@ public class CountWords {
         String words[] = s.split("\\s+");
         return words.length;
         // StringTokenizer tokens = new StringTokenizer(s);
-        // return tokens.countTokens();
+        // return tokens.countTokens;
     }
     public static void main(String[] args) {
         String str = "This is a sample string";
