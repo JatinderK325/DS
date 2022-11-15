bb2da9ee9889f8d00f3f6ca7c02a01f38694f476
JatinderK325 <jatinder1994k@gmail.com>
2022-11-14 22:54:59 -0500

    Comments added

--- OOPS3/AbstractClassAndFunction/BMW.java
+++ OOPS3/AbstractClassAndFunction/BMW.java
@@ -1,7 +1,7 @@
 package OOPS3.AbstractClassAndFunction;
 
 public class BMW extends Car{
-
+// BMW is extending a abstract class i.e Car so we need to override all the functions that Car has.
     @Override
     public String getCompany() {
         return null;
--- OOPS3/AbstractClassAndFunction/Car.java
+++ OOPS3/AbstractClassAndFunction/Car.java
@@ -1,7 +1,11 @@
 package OOPS3.AbstractClassAndFunction;
 
 public abstract class Car extends Vehicle {
+// Car is extending a abstract class i.e Vehicle so either we need to override all the functions that Vehicle has implemented or we need to declare Car as abstract as well.
 
+// Here Car's parent class 'Vehicle' class has two methods:
+// 1. public abstract boolean isMotorized(); 2. public abstract String getCompany(); 
+// Here in this class we just implemented only one function of parent class therefore we needed to declare 'Car' as abstract too. But if we implement both the functions of parent class here, we do'nt need to declare 'Car' as a abstract.
     @Override
     public boolean isMotorized() {
         return false;
