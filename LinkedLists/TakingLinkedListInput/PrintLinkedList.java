10df4b9c7e77c6ecff319ad163b0064c7abb42b8
JatinderK325 <jatinder1994k@gmail.com>
2022-11-16 22:07:20 -0500

    LinkedList program completed

--- LinkedLists/TakingLinkedListInput/PrintLinkedList.java
+++ LinkedLists/TakingLinkedListInput/PrintLinkedList.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class PrintLinkedList {
     public static Node<Integer> takeInput(){
-        Node<Integer> head = null; // head is null initially.
+        Node<Integer> head = null, tail = null; // head is null initially.
         Scanner s = new Scanner(System.in);
         System.out.println("Enter elements");
         int data = s.nextInt(); // taking elements from user
@@ -14,16 +14,24 @@ public class PrintLinkedList {
             Node<Integer> newNode = new Node<>(data);
             if(head == null){
                 head = newNode;
+                tail = newNode;
             }
             else{
+                /* 
                 // making links between nodes
                 Node<Integer> temp = head;
+                // this is for reaching that node with which we will insert our new node i.e for example at element 2's next, we are inserting reference for 3 element .
                 while(temp.next != null){
                     temp = temp.next; // moving temp
                 }
-                temp.next  = newNode;
+                temp.next  = newNode; // if temp.next == null
+                */
+                // OR
+                tail.next = newNode; // means we will put new element's reference at the tail's next part(last node's next).
+                tail = newNode; // or tail = tail.next; // updating tail becoz new element has inserted and that will be our new tail.
             }
-            data = s.nextInt();
+
+            data = s.nextInt(); // setting data step by step that is entered by user.
         }
         return head;
     }
